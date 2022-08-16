3adfe3ab2a219cdb6886d5eb44b6916ee3b8a392
Thuong <ngdinhthuong@gmail.com>
2022-08-16 09:31:30 +0700

    edit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import './App.css';
 import { Outlet} from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+import {useDispatch} from 'react-redux';
+import { useEffect } from 'react';
+import { getStaffs } from './components/staff/staffSlice';
 
 
 function App() {
+
+  const dispatch = useDispatch()
+  useEffect(() => {
+      dispatch(getStaffs())
+  }, [dispatch])
+
   return (
     <div className="App">
     <Navbar/>
--- src/components/staff/Staff.js
+++ src/components/staff/Staff.js
@@ -45,3 +45,56 @@ const Staff = () => {
 };
 
 export default Staff;
+
+
+
+// import React from 'react';
+// // import { STAFFS } from './staffs';
+// import { Link } from 'react-router-dom';
+// import SearchStaff from './SearchStaff';
+// import { useSelector } from 'react-redux'; 
+// import AddStaff from './AddStaff';
+// // import { getStaffs } from './staffSlice';
+
+// const Staff = () => {
+
+ 
+
+//     const data = useSelector(state => state.staffStoreReducer) // get intial state from reducer of store 
+
+//     const {staff1, status} = data
+
+//      console.log(staff1) // array staff
+
+//       const showListStaff = staff1?.map(staff => {
+//         return (
+//             <div className="col-md-2 col-sm-3 mt-4 mb-4" key={staff.id}>
+//               <Link to={`/staff/${staff.id}`} >
+//               <div className="card">
+//                 <img className="card-img-top" src={staff.image} alt="" />
+//                     <div className="card-body">
+//                             <h4 className="card-title">{staff.name}</h4>
+//                             <p className="card-text">Phòng ban</p>
+//                     </div>
+//                 </div>
+//               </Link>
+//             </div>
+//         )
+//     })
+
+//     return (
+//         <div className="container-fluid staff">
+//         <div className="row">
+//         <SearchStaff/>
+//         <AddStaff/>
+        
+//         </div>
+//         <div className="row"> 
+              
+//          {status !== 'loading' && showListStaff}
+//         </div>
+//         </div>
+//     );
+// };
+
+// export default Staff;
